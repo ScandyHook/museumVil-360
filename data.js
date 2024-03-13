a7f682909a9fed0a317e7a7f20395ce08349dd6a
ScandyHook <krushscandy@gmail.com>
2024-03-13 04:42:38 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,411 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_3978",
+      "name": "IMG_3978",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7201141767213617,
+          "pitch": 0.4184110127226255,
+          "rotation": 0,
+          "target": "1-img_3979"
+        },
+        {
+          "yaw": 0.9154715735577117,
+          "pitch": 0.40735544884690356,
+          "rotation": 0,
+          "target": "2-img_3980"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05263319062914107,
+          "pitch": 0.16708453840155002,
+          "title": "Title",
+          "text": "<span style=\"color: rgb(255, 255, 255); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: pre-wrap; background-color: rgb(135, 116, 225); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;\">Макет школы постройки\n1958 года\nВыполнили:\nученики 11 - х классов (выпуск 2004 года)\n</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-img_3979",
+      "name": "IMG_3979",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6681031485880027,
+          "pitch": 0.20841809078493156,
+          "rotation": 0,
+          "target": "0-img_3978"
+        },
+        {
+          "yaw": 1.407658947720579,
+          "pitch": 0.3253723979655625,
+          "rotation": 0,
+          "target": "7-img_3985"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.006940688692759878,
+          "pitch": -0.594896911466563,
+          "title": "Title",
+          "text": "<span style=\"color: rgb(255, 255, 255); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: pre-wrap; background-color: rgb(135, 116, 225); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;\">Гимн МБОУ СШ Nº2\nМузыка и слова Н. Шевчук\n\nСтоит в Рыбачем школа Nº2,\nЗдесь мы растём, мир новый узнавая,\nНам на всю жизнь она одна нужна,\nТа школа, что зовут её вторая.\nСчитаем мы за доблесть и за честь, И нам, друзья, другой судьбы не надо.\nЧто номер 2 такая школа есть,\nДля нас она и радость, и отрада.\nУж 50, как создана она,\nРастут в ней дети, мир свой познавая,\nДа будут весны юность и весна,\nДа здравствует любимая вторая.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-img_3980",
+      "name": "IMG_3980",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4353741682784538,
+          "pitch": 0.34970463720151557,
+          "rotation": 0,
+          "target": "0-img_3978"
+        },
+        {
+          "yaw": -1.3440768283348437,
+          "pitch": 0.5521712599769977,
+          "rotation": 0,
+          "target": "3-img_3981"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.34549066865249856,
+          "pitch": 0.5190303961595717,
+          "title": "Title",
+          "text": "Виниловый проигрыватель<br>"
+        },
+        {
+          "yaw": -0.9552375081688886,
+          "pitch": 0.3614772790157321,
+          "title": "Title",
+          "text": "Печатная машинка<br>"
+        }
+      ]
+    },
+    {
+      "id": "3-img_3981",
+      "name": "IMG_3981",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3898276510859127,
+          "pitch": 0.5785023235994569,
+          "rotation": 0,
+          "target": "2-img_3980"
+        },
+        {
+          "yaw": -1.366459446379423,
+          "pitch": 0.338138271375664,
+          "rotation": 0,
+          "target": "4-img_3982"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7198236702793217,
+          "pitch": -0.3177928159626138,
+          "title": "Title",
+          "text": "<span style=\"color: rgb(255, 255, 255); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: pre-wrap; background-color: rgb(135, 116, 225); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;\">25 июня 1969 года\nна основании постановления исполнительного комитета Второго\nПетропавловск - Камчатского городского Совета депутатов трудящихся - переименована в среднюю школу Nº2</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-img_3982",
+      "name": "IMG_3982",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5679566807307719,
+          "pitch": 0.5444188028508883,
+          "rotation": 0,
+          "target": "3-img_3981"
+        },
+        {
+          "yaw": -2.832063050039597,
+          "pitch": 0.515251910164956,
+          "rotation": 0,
+          "target": "5-img_3983"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.003629877749347088,
+          "pitch": -0.3784119758161957,
+          "title": "Title",
+          "text": "<span style=\"color: rgb(255, 255, 255); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: pre-wrap; background-color: rgb(135, 116, 225); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;\">1958 год\n«Средняя трудовая политехническая школа Nº7»\nЕлизовского района\nКамчатской области</span>"
+        }
+      ]
+    },
+    {
+      "id": "5-img_3983",
+      "name": "IMG_3983",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.186592117440437,
+          "pitch": 0.41587798487660166,
+          "rotation": 0,
+          "target": "4-img_3982"
+        },
+        {
+          "yaw": -1.4876038081190543,
+          "pitch": 0.28527016528486016,
+          "rotation": 0,
+          "target": "6-img_3984"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.016764176456854685,
+          "pitch": -0.4747676949071167,
+          "title": "Title",
+          "text": "<span style=\"color: rgb(255, 255, 255); font-family: Roboto, -apple-system, &quot;apple color emoji&quot;, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Oxygen-Sans, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: pre-wrap; background-color: rgb(135, 116, 225); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;\">ВИЛКОВ\nНиколай Александрович\n9.12.1918 - 18.08.1945\n\nКомандир взвода мерской пехоты. Герой Советского Союза, старшина первой статьи.\n\nПосмертно награжден званием Героя Советского Союза.\nВ июне 2015 года в г. Вилючинск установлена мемориальная доска с именем Вилкова НА.\nВ гоподе Вилючинск в микрорайоне Рыбачий, названа улица именем Н.А. Вилкова.</span>"
+        }
+      ]
+    },
+    {
+      "id": "6-img_3984",
+      "name": "IMG_3984",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1403763806249554,
+          "pitch": 0.3559560476383776,
+          "rotation": 0,
+          "target": "5-img_3983"
+        },
+        {
+          "yaw": -1.437246041471825,
+          "pitch": 0.4060539140649997,
+          "rotation": 0,
+          "target": "7-img_3985"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-img_3985",
+      "name": "IMG_3985",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.35483585530028,
+          "pitch": 0.3535607784267807,
+          "rotation": 0,
+          "target": "6-img_3984"
+        },
+        {
+          "yaw": -1.3668426178992483,
+          "pitch": 0.4666529961997714,
+          "rotation": 0,
+          "target": "1-img_3979"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
